5a696d2c6f1e7eec2bcad63b399e7ec914745c70
Alberto Muñoz <alberto.mqo@gmail.com>
2016-05-25 18:00:05 +0200

    Change Card and Poker to improve logic

--- src/poker/Card.java
+++ src/poker/Card.java
@@ -16,6 +16,7 @@ public final class Card {
     }
 
     public static final String STRING_RANK_CARDS = "23456789TJQKA";
+
     public static enum Rank {
         TWO, TRHEE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
     }
@@ -23,6 +24,12 @@ public final class Card {
     private final Rank rank;
 
     public Card(Suit suit, Rank rank) {
+        if (suit == null) {
+            throw new IllegalArgumentException("suit no puede tener un valor nulo");
+        }
+        if (rank == null) {
+            throw new IllegalArgumentException("rank no puede tener un valor nulo");
+        }
         this.suit = suit;
         this.rank = rank;
     }
@@ -34,7 +41,24 @@ public final class Card {
     public Rank getRank() {
         return rank;
     }
-    
+
+    @Override
+    public int hashCode() {
+        return rank.ordinal() * Suit.values().length + suit.ordinal();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = true;
+        if (this != obj) {
+            result = false;
+            if (obj != null && getClass() == obj.getClass()) {
+                result = hashCode() == ((Card) obj).hashCode();
+            }
+        }
+        return result;
+    }
+
     @Override
     public String toString() {
         int rankValue = rank.ordinal();
--- src/poker/Poker.java
+++ src/poker/Poker.java
@@ -5,6 +5,8 @@
  */
 package poker;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,11 +16,29 @@ import org.slf4j.LoggerFactory;
  */
 public class Poker {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Poker.class);
-    
+    private static final Logger LOGGER = LoggerFactory.getLogger(Poker.class);
+
+    private static void insert(Set<Card> cards, Card card) {
+        if (!cards.contains(card)) {
+            LOGGER.info("insertamos la carta: {}", card);
+            cards.add(card);
+        } else {
+            LOGGER.info("la carta: {} ya estaba en el conjunto", card);
+        }
+    }
+
     public static void main(String[] args) {
-        Card card = new Card(Card.Suit.CLUB, Card.Rank.ACE);
-        LOG.info("As de tréboles: {}", card);
+        Set<Card> cards = new HashSet<>();
+        Card[] cards2Insert = {
+            new Card(Card.Suit.CLUB, Card.Rank.ACE),
+            new Card(Card.Suit.CLUB, Card.Rank.TWO),
+            new Card(Card.Suit.CLUB, Card.Rank.TRHEE),
+            new Card(Card.Suit.CLUB, Card.Rank.ACE),
+            //new Card(Card.Suit.CLUB, null)
+        };
+        for (Card card : cards2Insert) {
+            insert(cards, card);
+        }
     }
 
 }
